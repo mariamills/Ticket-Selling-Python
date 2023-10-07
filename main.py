61657b9a5e3aa20730e72d82226ba499829acfa9
Maria Mills <cxwinterx@gmail.com>
2023-10-07 10:52:01 -0400

    Improve up file structure, add few more code comments

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import customtkinter as ctk
-from GUI.Login import Login
+from frames.login import Login
 
 class AppState:
     """class to hold the state of the application"""
@@ -22,15 +22,16 @@ def switch_frame(frame_name):
 # initialize app state
 app_state = AppState()
 
-# set up the root window
+# set up the root window appearance and theme
 ctk.set_appearance_mode("System")
 ctk.set_default_color_theme("blue")
 
+# create the root window and set its title and size
 root = ctk.CTk()
 root.title("Ticket System")
 root.geometry("500x350")
 
-# main container for frames
+# main container for frames (packs into root)
 container_frame = ctk.CTkFrame(root)
 container_frame.pack(fill='both', expand=True)
 
